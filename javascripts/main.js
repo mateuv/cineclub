8ded548dda8e33151b43c268c92992352892345f
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2016-04-25 22:49:51 -0500

    added knockout and some logic

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,35 @@
-$(function () {
-	$.get({
-		url: 'https://raw.githubusercontent.com/mateuv/cineclub/master/pelis.json',
-		success: function (data) {
-			console.log(data);
-		},
-		error: function (xhr) {
-			console.log(xhr);
-		}
+function MainViewModel() {
+	var self = this;
+
+	self.items  = ko.observableArray([]);
+	self.filter = ko.observable('');
+	self.shownItems = ko.pureComputed(function () {
+		return self.items().filter(function (item, index) {
+			if (self.filter()) {
+				return item.fqn.match(self.filter().toLowerCase());
+			} else {
+				return index < 10;
+			}
+		});
 	});
+
+	self.load = function (done) {
+		$.ajax({
+			url: '/pelis.jsonp',
+			jsonp: 'loadMovieData',
+			dataType: 'jsonp',
+			crossDomain: true,
+			success: done,
+			error: done
+		});
+	};
+}
+
+$(function () {
+	window.mvm = new MainViewModel();
+
+	mvm.load(function () {
+		ko.applyBindings(mvm);
+	});
+
 });
