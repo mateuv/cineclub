703dc8627bf2470102160b625d3a3551fd9223e4
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2016-04-25 23:05:24 -0500

    load jsonp from github

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,15 @@
+function Movie(data) {
+	var self = this;
+
+	self.title          = ko.observable(data.title);
+	self.original_title = ko.observable(data.original_title);
+	self.director       = ko.observable(data.director);
+	self.year           = ko.observable(data.year);
+	self.country        = ko.observable(data.country);
+	self.date           = ko.observable(data.date);
+	self.fqn            = ko.observable(data.fqn);
+}
+
 function MainViewModel() {
 	var self = this;
 
@@ -13,9 +25,15 @@ function MainViewModel() {
 		});
 	});
 
+	self.loadItems = function (items) {
+		self.items(items.map(function (item) {
+			return new Movie(item);
+		}));
+	};
+
 	self.load = function (done) {
 		$.ajax({
-			url: '/pelis.jsonp',
+			url: 'https://raw.githubusercontent.com/mateuv/cineclub/master/pelis.jsonp',
 			jsonp: 'loadMovieData',
 			dataType: 'jsonp',
 			crossDomain: true,
