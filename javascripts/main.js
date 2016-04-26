f2cc01f4c3e7433b2f98a3bda98406f2613879ce
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2016-04-25 23:13:35 -0500

    missing function call

--- javascripts/main.js
+++ javascripts/main.js
@@ -24,7 +24,7 @@ function MainViewModel() {
 	self.shownItems = ko.pureComputed(function () {
 		return self.items().filter(function (item, index) {
 			if (self.filter()) {
-				return item.fqn.match(self.filter().toLowerCase());
+				return item.fqn().match(self.filter().toLowerCase());
 			} else {
 				return index < 10;
 			}
